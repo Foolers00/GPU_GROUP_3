f227622228911b939b5c3c0850a73baf0e3d24c5
Thomas <e11909438@student.tuwien.ac.at>
2023-06-06 11:17:54 +0000

    implemented time measurements for minmax and max_distance

--- main.cu
+++ main.cu
@@ -10,17 +10,17 @@
 
 
 
-int main(){
-
-    // Point_array* points = test_sequence_4_1();// generate_random_points(8, -200, 800);
-    // Hull* hull = quickhull(points);
-
-    //test_sequence_2();
-    //test_sequence_5();
-
-    //test_minmax_cuda();
-
-    validate_minmax();
-
-    return 0;
-}
+//int main(){
+//
+//    // Point_array* points = test_sequence_4_1();// generate_random_points(8, -200, 800);
+//    // Hull* hull = quickhull(points);
+//
+//    //test_sequence_2();
+//    //test_sequence_5();
+//
+//    //test_minmax_cuda();
+//
+//    validate_minmax();
+//
+//    return 0;
+//}
